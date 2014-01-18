4df425279396765477252386df36d02dc41c0be2
marcinj <junczys@amu.edu.pl>
2014-01-18 12:10:10 +0100

    Commit local changes to Jekyll sources

--- buildNPush.sh
+++ buildNPush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -v
 
-git commit -a -m 'Commit local changes to Jekyll sources'
 jekyll build
+git commit -a -m 'Commit local changes to Jekyll sources'
 git checkout master
 rsync -a _site/* .
 rm -rf _site
