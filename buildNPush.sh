e488e955a9c0b6936feb37fe6acfa26952549af2
marcinj <junczys@amu.edu.pl>
2014-01-18 13:18:52 +0100

    Push static pages

--- buildNPush.sh
+++ buildNPush.sh
@@ -3,7 +3,7 @@
 jekyll build
 git commit -a -m 'Commit local changes to Jekyll sources'
 git checkout master
-rsync -a _site/* .
+rsync -ca _site/* .
 rm -rf _site
 git add *
 git commit -a -m 'Push static pages'
