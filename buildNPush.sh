89f1452e8e8a1dad94ac0c6d537f660908b74691
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-10-01 19:48:41 +0100

    fix

--- buildNPush.sh
+++ buildNPush.sh
@@ -3,10 +3,10 @@
 git commit -a -m 'Commit local changes to Jekyll sources'
 git up
 git push
-jekyll build
+bundle exec jekyll build
 git checkout master
-rsync -ca _site/* .
-rm -rf _site
+rsync -ca build/* .
+rm -rf build
 git add *
 git commit -a -m 'Commit static pages'
 git push
